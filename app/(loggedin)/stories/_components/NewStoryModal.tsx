54f85db72d22fceff3412b931cc6d64fa9ca3afe
Andy Pyle <andypyle@Andys-MacBook-Pro-2.local>
2023-10-07 11:50:54 -0700

    Add react select with chakra wrapper

--- app/(loggedin)/stories/_components/NewStoryModal.tsx
+++ app/(loggedin)/stories/_components/NewStoryModal.tsx
@@ -1,13 +1,14 @@
 'use client'
 
 import { Modal, type ModalProps } from '@/components/Modal'
+import { Select } from 'chakra-react-select'
 
 export const NewStoryModal: React.FC<Omit<ModalProps, 'children'>> = ({
   ...props
 }) => {
   return (
     <Modal title="New Story" size={{ base: 'full', md: '2xl' }} {...props}>
-      New story modal
+      <Select options={['one', 'two', 'three']} />
     </Modal>
   )
 }
